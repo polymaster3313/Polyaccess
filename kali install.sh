8fcd7a866fc142151fca286fd05cd335eead23a4
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-06-01 14:54:09 +0200

    Update kali install.sh

--- kali install.sh
+++ kali install.sh
@@ -9,5 +9,9 @@ sudo apt install python3.11
 # Install pip for Python 3.11
 sudo apt install python3.11-pip
 
+# change into requirements dir
+
+cd malware
+
 # Install the requirements using pip
 pip install -r requirements.txt
